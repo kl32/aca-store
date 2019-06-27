3a0b95545009f7882c16ea113c1a3eadfd48ff91
Kiley Lewis <kileylewis32@gmail.com>
2019-06-26 19:35:53 -0500

    init

--- public/index.js
+++ public/index.js
@@ -1,2 +1,28 @@
-//stuff
-//more stuff
+//Show the product name, rating, number of reviews and price.
+
+function Products(products) {
+
+let productDivs = "";
+
+for(let i=0; i < products.length; i++){
+    let product = products[i];
+    productDivs += 
+    `<h3>${product.name}</h3> 
+    <h4>${product.description}</h4>
+    <div>rating: ${product.rating}</div>
+    <div>price: ${product.price}</div>
+    <div>reviews: ${product.reviews.length}</div>
+    `
+}
+
+document.getElementById("productHere").innerHTML = productDivs;
+
+}
+
+Products(products);
+
+function search() {
+    let searchWord = document.getElementById("search").value;
+    let filteredProducts = products.filter(p=>p.name === searchWord);
+    Products(filteredProducts);
+}
